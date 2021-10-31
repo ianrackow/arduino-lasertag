c35e287e9d7f7b750f83c6762d005e0bfe48d4c7
Oasiris <teracontinens@gmail.com>
2021-10-31 16:32:41 -0400

    Added server/ and setup + run scripts

---
+++ server/run.bat
@@ -0,0 +1,4 @@
+:: NOTE: Don't run this before you install the pip requirements!
+set FLASK_APP=hello
+set FLASK_ENV=development
+flask run
---
+++ server/run.sh
@@ -0,0 +1,4 @@
+# NOTE: Don't run this before you install the pip requirements!
+export FLASK_APP=hello
+export FLASK_ENV=development
+flask run
