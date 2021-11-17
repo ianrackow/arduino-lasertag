7ce670f7a7644db9369faf243af39017e433784c
ANDY <knoblock.andy@gmail.com>
2021-11-16 20:50:00 -0500

    added .gitignore and deleted build folder from git

--- leaderboard/backend/routes/score.ts
+++ leaderboard/backend/routes/score.ts
@@ -1,39 +1,45 @@
 import { createHash } from "crypto";
 import express from "express";
-import { existsSync, readFileSync, renameSync, writeFileSync } from "fs";
+import {
+  copyFileSync,
+  existsSync,
+  readFileSync,
+  renameSync,
+  writeFileSync,
+} from "fs";
 import path, { resolve } from "path";
 
 type Player = { taggedBy: string[] };
 type Players = { [id: string]: Player | undefined };
 
-let game;
-
-const gamePath = "../data/current-game.json";
-
 const router = express.Router();
-const dataDir = path.normalize("../data");
-const currentGame = path.join(dataDir, "current-game.json");
-const pastGamesDir = path.join(dataDir, "past-games");
 
 router.get("/", (_, res) => {
   res.json({ msg: "🙆" });
 });
 
+const currentGameFile = path.posix.join("data", "current-game.json");
+const pastGamesDir = path.posix.join("data", "past-games");
+
 router.get("/start", (_, res) => {
-  console.log("data_dir", dataDir, currentGame);
-
   // if there is a current game, rename it to the hash of the file cuz I don't know how else to generate a unique filename
-  if (existsSync(currentGame)) {
-    const data = readFileSync(currentGame);
-    const hashSum = createHash("sha256");
-    hashSum.update(data);
+  if (existsSync(currentGameFile)) {
+    const data = readFileSync(currentGameFile);
 
-    const currentGameHash = hashSum.digest("hex");
+    // if there is data, copy it to past games just for funsies
+    if (data) {
+      const hashSum = createHash("sha256");
+      hashSum.update(data);
 
-    renameSync(currentGame, path.join(dataDir, currentGameHash));
+      const currentGameHash = hashSum.digest("hex");
+
+      copyFileSync(currentGameFile, path.join(pastGamesDir, currentGameHash));
+    }
   } else {
     console.log("lmao current game no exist stupid");
   }
+
+  writeFileSync(currentGameFile, JSON.stringify({}));
 
   res.sendStatus(200);
 });
@@ -49,15 +55,13 @@ router.get("/hit", (req, res) => {
   const taggerId = req.query.taggerId as string | undefined;
 
   if (typeof id === "string" && typeof taggerId === "string") {
-    const data = (require("../../data/current-game.json") ?? {}) as Players;
+    // current-game.json could be empty, so return an empty array if true
+    const data = (require("../../data/current-game.json") || {}) as Players;
 
     const { taggedBy } = data[id] ?? { taggedBy: [] };
     const newPlayerData: Player = { taggedBy: [...taggedBy, taggerId] };
 
     const newData: Players = { ...data, [id]: newPlayerData };
-
-    console.log("data", data);
-    console.log("other path", resolve("data/current-game.json"));
 
     writeFileSync("data/current-game.json", JSON.stringify(newData));
 
